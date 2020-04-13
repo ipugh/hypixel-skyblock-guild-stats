84ab6f4c4ba03497d3c47718b262fe59a99adc1b
Isaac Pugh <me@isaacpugh.com>
2020-04-13 02:16:55 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Hypixel Skyblock Guild Stats
+
+This script lets you put your guild members in a spreadsheet and then get their stats
+
+# Usage
+
+## Set the environment variables
+1. `HYPIXEL_API_KEY` is set to your hypixel api key. Do `/api` in-game to get this value
+2. `STATS_SHEETS_ID` is set to the id of your google spreadsheet. Look in the url for this id
+
+### Setting environment variables in Windows
+In command prompt, `setx HYPIXEL_API_KEY thisistheapikeyhere` to set a variable. Close command prompt and open again in order for this change to take effect.
+
+### Setting environment variables in Linux and MacOS
+Enter your `~/.bashrc` or `~/.zshrc` file and at the bottom put in `export HYPIXEL_API_KEY=thisistheapikeyhere` to set an environment variable.
+
+## Set up the Google Sheet
+1. Run the program once to set up the api connection with Google Sheets
+2. The values will start to be filled at the 3rd row of the sheet, freeze the first two rows and use them for a header. A header is located in the **About** section of this page.
+3. Fill in the first two columns with the username and profile of each member
+
+## About
+The Third column `Failed?` is for determining whether the api call was a success. If this value ends up with `T` after the program is run, most likely that member's API settings are turned off. Occasionally a member's API settings are on, but the API call failed for some reason. Running the program again should fix this. Changing the `check_failure` variable near the top of `main.py` to `True` will only get the API results from members flagged with `T`.
+
+Header:
+`Name, Profile, Failed?, Skill Average, Slayer XP, Combat, Foraging, Mining, Fishing, Farming, Alchemy, Enchanting, Zombie, Spider, Wolf, Gifts, Given, Deaths, Purse`
+
+# TODO
+- The rows should be more customizable to the user, make a file to store the format and provide some basic formats
+- Add more values that can be used in the spreadsheet
+- Organize code into separate files and comment functions
+- Add `-f` flag to only run failed users, currently this is just a boolean in the code
+- Remove magic numbers
---
+++ main.py
@@ -0,0 +1,345 @@
+import time
+
+import httpx
+import asyncio
+import json
+import base64
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import sys
+
+# TODO:
+# implement '-f' flag just to update ones that are flagged
+# money in bank?
+check_failure = False
+
+api_key =  os.environ.get('HYPIXEL_API_KEY')
+spreadsheet_id = os.environ.get('STATS_SHEETS_ID')
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+xp_needed = {
+    0: 0,
+    1: 50,
+    2: 175,
+    3: 375,
+    4: 675,
+    5: 1175,
+    6: 1925,
+    7: 2925,
+    8: 4425,
+    9: 6425,
+    10: 9925,
+    11: 14925,
+    12: 22425,
+    13: 32425,
+    14: 47425,
+    15: 67425,
+    16: 97425,
+    17: 147425,
+    18: 222425,
+    19: 322425,
+    20: 522425,
+    21: 822425,
+    22: 1222425,
+    23: 1722425,
+    24: 2322425,
+    25: 3022425,
+    26: 3822425,
+    27: 4722425,
+    28: 5722425,
+    29: 6822425,
+    30: 8022425,
+    31: 9322425,
+    32: 10722425,
+    33: 12222425,
+    34: 13822425,
+    35: 15522425,
+    36: 17322425,
+    37: 19222425,
+    38: 21222425,
+    39: 23322425,
+    40: 25522425,
+    41: 27822425,
+    42: 30222425,
+    43: 32722425,
+    44: 35322425,
+    45: 38072425,
+    46: 40972425,
+    47: 44072425,
+    48: 47472425,
+    49: 51172425,
+    50: 55172425,
+}
+
+
+async def get_profile(uuid):
+    url = 'https://api.hypixel.net/player'
+    print(".", end="")
+    response = httpx.get(
+        url,
+        params={'key': api_key, 'uuid': uuid},
+    )
+    json = response.json()
+    return json
+
+
+async def get_skyblock(profile):
+    url = 'https://api.hypixel.net/skyblock/profile'
+    print(".", end="")
+    response = httpx.get(
+        url,
+        params={'key': api_key, 'profile': profile},
+    )
+    json = response.json()
+    return json
+
+
+async def get_uuid(name):
+    url = "https://api.mojang.com/users/profiles/minecraft/" + name
+    response = httpx.get(url)
+    return response
+
+
+def get_profile_id(profile, name):
+    profiles = profile['player']['stats']['SkyBlock']['profiles']
+    for p in profiles:
+        print(p)
+        print(profiles[p]['cute_name'])
+        print(str(name).lower)
+        if str(profiles[p]['cute_name']).lower() == name.lower():
+            return str(p)
+
+    return None
+
+
+def get_stats(name, cutename):
+    name = asyncio.run(get_uuid(name))
+    id = name.json()['id']
+    print(id)
+
+    profile = asyncio.run(get_profile(id))
+    print(profile['player']['stats']['SkyBlock'])
+
+    profile_id = get_profile_id(profile, cutename)
+    print("This is the profile: ", profile_id)
+
+
+    response = asyncio.run(get_skyblock(profile_id))
+    stats = response['profile']['members'][id]  # this should be the data
+    return stats
+
+
+def get_level(xp):
+    if xp == 0:
+        return 0
+    lvl = -1
+    for i in range(51):
+        if xp < xp_needed[i]:
+            lvl = i - 1
+            break
+
+    if lvl == -1:
+        return 50
+
+    xp = xp - xp_needed[lvl]
+    current_xp = xp_needed[lvl]
+    next_xp = xp_needed[lvl + 1]
+    level_cost = next_xp - current_xp
+    progress = round(xp / level_cost, 2)
+    lvl = lvl + progress
+    return lvl
+
+
+def spreadsheets():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # A2:A gets names
+    # B2:B gets profiles
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=spreadsheet_id,
+                                range="A3:A").execute()
+    names = result.get('values', [])
+
+    result = sheet.values().get(spreadsheetId=spreadsheet_id,
+                                range="B3:B").execute()
+    profiles = result.get('values', [])
+
+    result = sheet.values().get(spreadsheetId=spreadsheet_id,
+                                range="C3:C").execute()
+    failure = result.get('values', [])
+
+
+    return (names, profiles, failure)
+
+def insert_spreadsheet(user_data, row):
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    values = [
+        ["",
+        user_data['skill_average'],
+        user_data['total_slayer'],
+        user_data['combat'],
+        user_data['foraging'],
+        user_data['mining'],
+        user_data['fishing'],
+        user_data['farming'],
+        user_data['alchemy'],
+        user_data['enchanting'],
+        user_data['zombie'],
+        user_data['spider'],
+        user_data['wolf'],
+        user_data['gifts_given'],
+        user_data['death_count'],
+        user_data['coin_purse']]
+    ]
+
+    body = {
+        'values': values
+    }
+
+    rangename = 'C' + str(row) + ':R' + str(row)
+
+    result = service.spreadsheets().values().update(
+        spreadsheetId=spreadsheet_id, range=rangename,
+        valueInputOption="RAW", body=body).execute()
+
+
+def fail_spreadsheet(row):
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    values = [
+        ['T']
+    ]
+
+    body = {
+        'values': values
+    }
+
+    rangename = 'C' + str(row)
+
+    result = service.spreadsheets().values().update(
+        spreadsheetId=spreadsheet_id, range=rangename,
+        valueInputOption="RAW", body=body).execute()
+
+
+members = spreadsheets()
+
+for i in range(len(members[0])):
+
+    if (check_failure and len(members[2][i]) == 0):
+        continue
+
+    #stats = get_stats("spysick99", "Zucchini")
+    user_data = {}
+    print(members[0][i], members[1][i])
+    try:
+        stats = get_stats(members[0][i][0], members[1][i][0])
+        #print(stats)
+        #for key in stats:
+        #    print(key)
+        user_data['foraging'] = get_level(stats['experience_skill_foraging'])
+        user_data['farming'] = get_level(stats['experience_skill_farming'])
+        user_data['alchemy'] = get_level(stats['experience_skill_alchemy'])
+        user_data['combat'] = get_level(stats['experience_skill_combat'])
+        user_data['enchanting'] = get_level(stats['experience_skill_enchanting'])
+        user_data['mining'] = get_level(stats['experience_skill_mining'])
+        user_data['fishing'] = get_level(stats['experience_skill_fishing'])
+        user_data['skill_average'] = round(
+            (user_data['foraging'] + user_data['farming'] + user_data['alchemy'] + user_data['combat'] +
+             user_data['enchanting'] + user_data['mining'] + user_data['fishing']) / 7, 2)
+        user_data['zombie'] = stats['slayer_bosses']['zombie']['xp']
+        user_data['spider'] = stats['slayer_bosses']['spider']['xp']
+        user_data['wolf'] = stats['slayer_bosses']['wolf']['xp']
+        user_data['total_slayer'] = user_data['zombie'] + user_data['spider'] + user_data['wolf']
+        user_data['death_count'] = stats['death_count']
+        user_data['coin_purse'] = stats['coin_purse']
+        user_data['gifts_given'] = stats['stats']['gifts_given']
+    except:
+        fail_spreadsheet(i+3)
+        print("User failed!!!")
+        time.sleep(3)
+        continue
+
+
+    """
+    print("foraging: ", foraging)
+    print("farming: ", farming)
+    print("alchemy: ", alchemy)
+    print("combat: ", combat)
+    print("enchanting: ", enchanting)
+    print("mining: ", mining)
+    print("fishing: ", fishing)
+    """
+
+
+    """
+    print("zombie: ", zombie)
+    print("spider: ", spider)
+    print("wolf: ", wolf)
+    print("total slayer:", total_slayer)
+    """
+
+    insert_spreadsheet(user_data, i+3)
+
+    time.sleep(3)
